0bca538b4b06c48cada0e456cb17c3174cba6297
kazimovzaman2 <kazimovzaman2@gmail.com>
2025-05-14 19:46:53 +0400

    Add data source for GitHub app installation and update workspace configuration

--- varaibles.tf
+++ varaibles.tf
@@ -2,3 +2,13 @@ variable "organization_name" {
   default = "kazimovzaman2-org"
   type    = string
 }
+
+variable "github_app_installation_id" {
+  default = 66848763
+  type    = number
+}
+
+variable "github_organization_name" {
+  default = "kazimovzaman2"
+  type    = string
+}
