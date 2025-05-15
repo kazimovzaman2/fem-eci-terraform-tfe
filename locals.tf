0c5f0f9f5053855f1ecb3932a029e3397df96565
kazimovzaman2 <kazimovzaman2@gmail.com>
2025-05-15 21:16:38 +0400

    Add GitHub workspace configuration to locals

--- locals.tf
+++ locals.tf
@@ -13,7 +13,7 @@ locals {
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
 
-    "fem-eci-tfe" = {
+    "fem-eci-github" = {
       description         = "Example automation workspace for GitHub resources."
       execution_mode      = "local"
       project_id          = module.project["fem-eci-project"].id
